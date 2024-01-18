319cce5512ec7b119ef3f6766aacbad8826c0614
Tomohisa Yamazoe <tomohisa.yamazoe@gmail.com>
2024-01-18 09:41:49 +0900

    Initial Commit

---
+++ Study2Nfc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study2Nfc
+//
+//  Created by Tomohisa Yamazoe on 2024/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
